82a87dffa6ddddd20a67d699f3d02b4d7df429f5
Chris Ailey <chris.ailey90@gmail.com>
2024-09-07 18:16:56 +0100

    continued building layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,26 @@ function App() {
 
   return (
     <div id='app-window' className='w-screen h-screen bg-green-500 flex justify-center items-center'>
-      <div id='game-window' className='max-w-[1920px] max-h-[1080px] w-full h-full bg-green-700'>
-        <div id='game-area' className='w-full h-full'>
-          <div id='deck'></div>
+      <div id='game-window' className='flex absolute max-w-[1920px] max-h-[1080px] w-full h-full bg-green-700 overflow-hidden'>
+        {/* <div id='game-area' className='flex w-full h-full'> */}
 
-          <div id='help'></div>
+          <div id='deck' className='absolute w-52 h-52 bg-white'></div>
+
+          <div id='help' className='absolute right-0 w-52 h-52 bg-white'></div>
           
-          <div id='greendale'></div>
+          <div id='community' className='m-auto bg-black h-fit w-fit flex flex-row'>
+            {[0,1,2,3,4].map((number, index) => (
+              <div key={index} className='w-28 h-40 bg-white mx-4'>
 
-          <div id='hole'></div>
+              </div>
+            ))}
+          </div>
 
-          <div id='chips'></div>
+          <div id='hole' className='absolute'></div>
 
+          <div id='chips' className='absolute bottom-0 h-52 w-52 bg-white'></div>
 
-
-        </div>
+        {/* </div> */}
 
       </div>
 
