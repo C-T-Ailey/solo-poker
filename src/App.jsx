d5a10fb1af40fa7a2dc749bc85f5e1bc18759a1e
Chris Ailey <chris.ailey90@gmail.com>
2024-09-12 19:18:16 +0100

    began implementation of functions and classes for creating a deck, populating it with cards, and drawing cards from the deck

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,113 @@ import './App.css'
 import Help from './components/Help'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  // state managing the help modal view
   const [showHelp, setShowHelp] = useState(false)
 
+  // state managing whether jokers have been unlocked for inclusion in deck generation
+  const [jokersUnlocked, setJokersUnlocked] = useState(false)
+
+  // state managing the contents of the deck
+  const [deck, setDeck] = useState([])
+
+  // useeffect hook for debugging the deck state
   useEffect(()=>{
-    console.log(showHelp)
-  },[showHelp])
+    console.log(deck)
+  },[deck])
+
+  // function for generating random integers
+  const randInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  // arrays containing the items for constructing cards
+  const suitsArray = ["Clubs","Diamonds","Hearts","Spades"];
+
+  const ranksArray = ["Joker","Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
+
+  // class defining an instance of a card
+  class Card {
+    constructor(suit, rank, value) {
+      this.suit = suit;
+      this.rank = rank;
+      this.value = value;
+    };
+    name() {
+      return `${this.rank} of ${this.suit}`
+    }
+  }
+
+  // unused test card
+  const spadesAce = new Card("Spades", "Ace", 1)
+
+  // unused class defining an instance of the deck
+  class Deck {
+    constructor(cards) {
+      this.cards = cards;
+    };
+    
+    draw() {
+      console.log(this.cards.pop())
+    }
+  }
+
+  // function for generating a card to include in the deck
+  const generateCard = () => {
+    let value = 0;
+    
+    const selectSuit = () => {
+      return suitsArray[randInt(0,3)];
+    }
+    
+    const selectRank = () => {
+      // if (!!jokersUnlocked) {
+      //   var selectedRank = randInt(0,13);
+      // } else {
+        var selectedRank = randInt(1,13);
+      // }
+
+        value = selectedRank;
+          
+        return ranksArray[selectedRank];
+    }
+        
+    const card = new Card(selectSuit(), selectRank(), value)
+        
+    return card
+  }
+      
+  // function for generating the deck
+  const generateDeck = () => {
+    let cards = []
+    let cardNames = []
+    
+    while (cards.length < 52) {
+      const newCard = generateCard();
+      
+      if (!cardNames.includes(newCard.name())){
+        cards.push(newCard);
+        cardNames.push(newCard.name());
+      }
+    }
+    
+    setDeck(cards);
+  }
+
+  // function for drawing a card from the deck
+  const drawCard = () => {
+    let deckCopy = Array.from(deck);
+
+    const drawnCard = deckCopy.pop();
+
+    setDeck(deckCopy);
+
+    console.log(drawnCard);
+
+    return drawnCard;
+  }
 
   return (
     <div id='app-window' className='w-screen h-screen bg-green-500 flex justify-center items-center'>
@@ -48,6 +149,12 @@ function App() {
           </div>
 
         {/* </div> */}
+
+          <div id="testbuttons" className='absolute bottom-52 flex flex-col justify-center w-full'>
+              <button onClick={() => generateDeck()}>Generate Deck</button>
+              <button onClick={() => drawCard()}>Draw Card</button>
+              {/* <button></button> */}
+          </div>
         
       <Help showHelp={showHelp} setShowHelp={setShowHelp} />
 
